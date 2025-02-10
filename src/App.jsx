61f268175f67859a2512c3f567e8540c64209ac4
Sachin Alam <sachinalam4216@gmail.com>
2025-02-10 21:13:00 +0530

    Final push

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { data } from "./data";
+
+const App = () => {
+  const [search, setSearch] = useState("");
+
+  const filteredData = search
+    ? data.filter(
+        (item) =>
+          item.first_name.toLowerCase().includes(search.toLowerCase()) ||
+          item.last_name.toLowerCase().includes(search.toLowerCase()) ||
+          item.email.toLowerCase().includes(search.toLowerCase()) ||
+          item.phone.includes(search)
+      )
+    : data;
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl text-center my-5">Contact Keeper</h1>
+      <div>
+        <input
+          className="w-full border border-slate-200 outline-none p-2 px-3 rounded"
+          type="text"
+          placeholder="Search Here"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
+      <div className="overflow-x-auto mt-7">
+        <table className="min-w-full border border-gray-300 bg-white shadow-md rounded-lg">
+          <thead className="bg-gray-100">
+            <tr>
+              <th className="px-4 py-2 border-b text-left text-gray-600">
+                First Name
+              </th>
+              <th className="px-4 py-2 border-b text-left text-gray-600">
+                Last Name
+              </th>
+              <th className="px-4 py-2 border-b text-left text-gray-600">
+                Email
+              </th>
+              <th className="px-4 py-2 border-b text-left text-gray-600">
+                Phone
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.length > 0 ? (
+              filteredData.map((contact) => {
+                return (
+                  <tr key={contact.id}>
+                    <td className="px-4 py-2 border-b text-left text-gray-600">
+                      {contact.first_name}
+                    </td>
+                    <td className="px-4 py-2 border-b text-left text-gray-600">
+                      {contact.last_name}
+                    </td>
+                    <td className="px-4 py-2 border-b text-left text-gray-600">
+                      {contact.email}
+                    </td>
+                    <td className="px-4 py-2 border-b text-left text-gray-600">
+                      {contact.phone}
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan="4" className="px-4 py-2 text-center text-gray-500">
+                  No results found
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default App;
